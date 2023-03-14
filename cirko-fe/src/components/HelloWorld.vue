2e78152e8dc710a5adc7f2416bce4b28af3f5c35
luka19517 <luka.djorovic3010@gmail.com>
2023-03-14 22:26:04 +0100

    configs

--- cirko-fe/src/components/HelloWorld.vue
+++ cirko-fe/src/components/HelloWorld.vue
@@ -1,7 +1,20 @@
 <template id="app">
   <h2>Hello world!</h2>
+  <h3>Message from backend {{ messageFromBackend() }}</h3>
 </template>
 
 <script setup>
+import axios from 'axios'
+
+async function messageFromBackend() {
+  const response = await axios({
+    method: 'get',
+    url: '/service/initialConnection'
+  })
+
+  console.log(response);
+  return response.data;
+}
+
 
 </script>
